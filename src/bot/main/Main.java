9984b8c0303252a00670ce762575586e4c23311e
mattia buffa <mattia.buffa-1@studenti.unitn.it>
2018-06-29 15:29:51 +0200

    repush

--- src/bot/main/Main.java
+++ src/bot/main/Main.java
@@ -24,7 +24,7 @@ public class Main {
 	
 	public static void main(String[] args) {	
         ApiContextInitializer.init();
-        
+       
         try {
         	myBot = new MyBot();
             telegramBotsApi.registerBot(myBot);
